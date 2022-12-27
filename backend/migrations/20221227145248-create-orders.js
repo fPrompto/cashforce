17644bb440665b14d25d8963e6719fb7adcc72a3
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2022-12-27 12:06:48 -0300

    :sparkles: feat: migration e modelda tabela orders criados

---
+++ backend/migrations/20221227145248-create-orders.js
@@ -0,0 +1,105 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      orderNfId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      orderNumber: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      orderPath: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      orderFileName: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      orderOriginalName: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      emissionDate: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      pdfFile: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      emitedTo: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      nNf: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      CTE: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      value: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      cnpjId: {
+        allowNull: true,
+        type: Sequelize.INTEGER(11),
+      },
+      userId: {
+        allowNull: true,
+        type: Sequelize.INTEGER(11),
+      },
+      buyerId: {
+        allowNull: true,
+        type: Sequelize.INTEGER(11),
+      },
+      providerId: {
+        allowNull: true,
+        type: Sequelize.INTEGER(11),
+      },
+      orderStatusBuyer: {
+        type: Sequelize.STRING,
+        defaultValue: '0',
+      },
+      orderStatusProvider: {
+        type: Sequelize.STRING,
+        defaultValue: '0',
+      },
+      deliveryReceipt: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      cargoPackingList: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      deliveryCtrc: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('orders');
+  }
+};
---
+++ backend/models/orders.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const Order = sequelize.define('Order', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    orderNfId: DataTypes.STRING,
+    orderNumber: DataTypes.STRING,
+    orderPath: DataTypes.STRING,
+    orderFileName: DataTypes.STRING,
+    orderOriginalName: DataTypes.STRING,
+    emissionDate: DataTypes.STRING,
+    pdfFile: DataTypes.STRING,
+    emitedTo: DataTypes.STRING,
+    nNf: DataTypes.STRING,
+    CTE: DataTypes.STRING,
+    value: DataTypes.STRING,
+    cnpjId: DataTypes.INTEGER(11),
+    userId: DataTypes.INTEGER(11),
+    buyerId: DataTypes.INTEGER(11),
+    providerId: DataTypes.INTEGER(11),
+    orderStatusBuyer: DataTypes.STRING,
+    orderStatusProvider: DataTypes.STRING,
+    deliveryReceipt: DataTypes.STRING,
+    cargoPackingList: DataTypes.STRING,
+    deliveryCtrc: DataTypes.STRING,
+  }, { tableName: 'orders' },
+  );
+
+  return Order;
+};
