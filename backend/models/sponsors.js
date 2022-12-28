4fb1910abd4019ae80b9596bab47b26fed498486
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2022-12-28 11:52:23 -0300

    :sparkles: feat: migration e modelda tabela sponsors criados

---
+++ backend/models/sponsors.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const Sponsor = sequelize.define('sponsors', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    tradingName: DataTypes.STRING,
+    cashforceTax: DataTypes.STRING,
+    responsibleName: DataTypes.STRING,
+    responsibleEmail: DataTypes.STRING,
+    responsiblePosition: DataTypes.STRING,
+    responsiblePhone: DataTypes.STRING,
+    responsibleMobile: DataTypes.STRING,
+    website: DataTypes.STRING,
+    postalCode: DataTypes.STRING,
+    address: DataTypes.STRING,
+    number: DataTypes.STRING,
+    complement: DataTypes.STRING,
+    neighborhood: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    bank: DataTypes.STRING,
+    bankAgency: DataTypes.STRING,
+    account: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+    situation: DataTypes.STRING,
+    situationDate: DataTypes.STRING,
+    cnpjId: DataTypes.INTEGER(11),
+    email: DataTypes.STRING,
+  }, { tableName: 'sponsors' },
+  );
+
+  return Sponsor;
+};
