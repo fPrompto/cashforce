c7a7cbe765a7f8fe2ce45f0316ecfca07700aa23
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-01-02 10:57:02 -0300

    :wrench: chore: Express configurado

---
+++ backend/config/strings.js
@@ -0,0 +1,15 @@
+const ROUTE = {
+  CASHFORCE: '/',
+};
+
+const STATUS_CODE = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  IM_A_TEAPOT: 418,
+};
+
+module.exports = {
+  ROUTE,
+  STATUS_CODE,
+};
---
+++ backend/routes/index.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+
+const cashForceRouter = Router();
+
+module.exports = cashForceRouter;
