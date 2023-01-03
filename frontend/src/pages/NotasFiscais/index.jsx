5800994bd972fcfab8456c6c9b65d379872bceb5
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-01-03 16:49:07 -0300

    :sparkles: feat: Página agora renderiza os dados orders

--- frontend/src/pages/NotasFiscais/index.jsx
+++ frontend/src/pages/NotasFiscais/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import SideBar from './components/SideBar';
 
@@ -7,6 +8,9 @@ import notasfiscais from '../../images/notasfiscais_black.png';
 import './style.css';
 
 function NotasFiscais() {
+  const [orderData, setOrderData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
   const columns = [
     'NOTA FISCAL',
     'SACADO',
@@ -16,7 +20,36 @@ function NotasFiscais() {
     'STATUS',
   ];
 
-  return (
+  const orderStatus = [
+    'Pendente de confirmação',
+    'Pedido confirmado',
+    'Não reconhece o pedido',
+    'Mercadoria não recebida',
+    'Recebida com avaria',
+    'Devolvida',
+    'Recebida com devolução parcial',
+    'Recebida e confirmada',
+    'Pagamento Autorizado',
+  ];
+
+  const getData = async () => {
+    setIsLoading(true);
+    const URL = 'http://localhost:7000/find/all/orders';
+    const response = await axios({
+      method: 'GET',
+      url: URL,
+    });
+    const data = await response.data;
+    console.log('data axios =>', data);
+    setOrderData(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData()
+  }, []);
+
+  return isLoading ? <span>Loading...</span> : (
     <div>
       <hr className="line" />
       <hr className="line line2" />
@@ -30,22 +63,42 @@ function NotasFiscais() {
           />
           <span className='notas-title'>Notas fiscais</span>
           <br />
-          <span className='notas-subtitle'>Visualize as notas fiscais que você tem.</span>
+          <span className='notas-subtitle'>
+            Visualize as notas fiscais que você tem.
+          </span>
         </div>
         <table>
           <thead>
             <tr>
-              {columns.map((name) => {
+              {columns.map((name, i) => {
                 return (
-                  <th className='table-head'>{name}</th>
+                  <th key={i} className='table-head'>{name}</th>
                 );
               })}
             </tr>
           </thead>
           <tbody>
-            <tr>
+            {/* <tr>
               <td>line 1</td>
-            </tr>
+            </tr> */}
+            {orderData.map((data, i) => {
+              const emissionDate = new Date(data.emissionDate);
+
+              const eDay = emissionDate.getDay();
+              const eMonth = emissionDate.getMonth();
+              const eYear = emissionDate.getFullYear();
+
+              return (
+                <tr key={i}>
+                  <td>{data.orderNfId}</td>
+                  <td>{data.buyer.name}</td>
+                  <td>{data.provider.name}</td>
+                  <td>{`${eDay}/${eMonth}/${eYear}`}</td>
+                  <td>{data.value}</td>
+                  <td>{orderStatus[+data.orderStatusBuyer]}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
