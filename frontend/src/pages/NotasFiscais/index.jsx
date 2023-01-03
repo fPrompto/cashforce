ea99dcc4f740456a821c38a1d8e141f6ea3d9bdf
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-01-03 14:50:50 -0300

    :lipstick: Página parcialmente pronta

--- frontend/src/pages/NotasFiscais/index.jsx
+++ frontend/src/pages/NotasFiscais/index.jsx
@@ -2,12 +2,53 @@ import React from 'react';
 
 import SideBar from './components/SideBar';
 
+import notasfiscais from '../../images/notasfiscais_black.png';
+
 import './style.css';
 
 function NotasFiscais() {
+  const columns = [
+    'NOTA FISCAL',
+    'SACADO',
+    'CEDENTE',
+    'EMISSÃO',
+    'VALOR',
+    'STATUS',
+  ];
+
   return (
     <div>
+      <hr className="line" />
+      <hr className="line line2" />
       <SideBar />
+      <div className='notas-list'>
+        <div className='title-div'>
+          <img
+            src={notasfiscais}
+            alt="Símbolo Notas fiscais"
+            className='notas-img-title'
+          />
+          <span className='notas-title'>Notas fiscais</span>
+          <br />
+          <span className='notas-subtitle'>Visualize as notas fiscais que você tem.</span>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              {columns.map((name) => {
+                return (
+                  <th className='table-head'>{name}</th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>line 1</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
