d8b20f1af31e562687ad8b8c088d7e094970d6d0
Fernando F Monteiro <fernando.monteiro.f11@gmail.com>
2023-01-03 10:38:15 -0300

    :lipstick: Ajustes no menu lateral

--- frontend/src/pages/NotasFiscais/components/SideBar.jsx
+++ frontend/src/pages/NotasFiscais/components/SideBar.jsx
@@ -5,11 +5,11 @@ import notasfiscais from '../../../images/notasfiscais.png';
 
 function SideBar() {
   return (
-    <div className="d-flex flex-column flex-shrink-0 p-3 cash-sidebar">
-      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+    <div className="cash-sidebar">
+      <a href="/">
         <img src={logo} alt="Logo CashForce" className='sidebar-title-img' />
       </a>
-      <ul className="nav nav-pills flex-column mb-auto">
+      <ul className='nav'>
         <li className="nav-item">
           <div className='nav-div'>
             <div className='nav-button'>
